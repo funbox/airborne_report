1c130ca67883d18edf6bb340f946dee564ae489e
d.efimov <d.efimov@fun-box.ru>
2017-01-13 16:28:28 +0400

    puts request and response

--- lib/report_airborne.rb
+++ lib/report_airborne.rb
@@ -3,3 +3,16 @@ require "report_airborne/version"
 module ReportAirborne
   # Your code goes here...
 end
+
+module Airborne
+  module RestClientRequester
+    alias_method :origin_make_request, :make_request
+
+    def make_request(*args)
+      response = origin_make_request(*args)
+      puts "request: #{response.request.inspect}"
+      puts "response: #{response}"
+      response
+    end
+  end
+end
