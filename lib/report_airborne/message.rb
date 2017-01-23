3a20914305bd0c2ff4c8be3a12dc1a3fa5dc0844
d.efimov <d.efimov@fun-box.ru>
2017-01-23 13:24:55 +0400

    improved Message

--- lib/report_airborne/message.rb
+++ lib/report_airborne/message.rb
@@ -2,26 +2,8 @@ require 'multi_json'
 
 module ReportAirborne
   class Message
-    def initialize(test)
-      @test = test
-    end
-
-    def save(location)
-      before_json = get_before_json
-      after_json = {
-        'tests' => before_json['tests'].merge(location => @test)
-      }
-      File.open('report.json', 'w') do |file|
-        file.write(MultiJson.dump(after_json))
-      end
-    end
-
-    def get_before_json
-      MultiJson.load(File.read('report.json'))
-    end
-
-    def self.get_after_tests(location, before_tests, request, response)
-      before_tests.merge(location => new_test(request, response))
+    def initialize(message)
+      @message = message
     end
 
     def self.full(request, response)
@@ -53,6 +35,19 @@ module ReportAirborne
           'body' => response
         }
       })
+    end
+
+    def save(location)
+      after_json = {
+        'tests' => before_json['tests'].merge(location => @message)
+      }
+      File.open('report.json', 'w') do |file|
+        file.write(MultiJson.dump(after_json))
+      end
+    end
+
+    def before_json
+      MultiJson.load(File.read('report.json'))
     end
   end
 end
