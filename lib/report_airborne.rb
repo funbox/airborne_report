607f0bbf40672e634a84d67710713fc2da1a823b
d.efimov <d.efimov@fun-box.ru>
2017-01-23 13:50:44 +0400

    add private methods

--- lib/report_airborne.rb
+++ lib/report_airborne.rb
@@ -12,24 +12,36 @@ module Airborne
 
     def make_request(*args)
       response = origin_make_request(*args)
-      if response.is_a?(RestClient::Response)
-        request = response.request
-        ReportAirborne::Message.full(request, response).save(location)
-      else
-        url = get_url(args[1])
-        ReportAirborne::Message.wasted(args, response, url).save(location)
-      end
+      save!(args, response)
       response
     rescue SocketError => error
-      url = get_url(args[1])
-      ReportAirborne::Message.wasted(args, response, url).save(location)
+      wasted_save(args, response)
       error
     end
 
     private
 
+    def save!(args, response)
+      if response.is_a?(RestClient::Response)
+        full_save(response)
+      else
+        wasted_save(args, response)
+      end
+    end
+
+    def full_save(response)
+      request = response.request
+      ReportAirborne::Message.full(request, response).save(location)
+    end
+
+    def wasted_save(args, response)
+      url = get_url(args[1])
+      ReportAirborne::Message.wasted(args, response, url).save(location)
+    end
+
     def location
       inspect.to_s.split('(').last.split(')').first
     end
+
   end
 end
