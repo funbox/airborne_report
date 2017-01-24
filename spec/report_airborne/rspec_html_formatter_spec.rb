9d7074848a39fdd0942a331afd7d7ddba5625c89
d.efimov <d.efimov@fun-box.ru>
2017-01-24 17:49:47 +0400

    improved rspec html formatter

--- spec/report_airborne/rspec_html_formatter_spec.rb
+++ spec/report_airborne/rspec_html_formatter_spec.rb
@@ -2,16 +2,32 @@ require 'spec_helper'
 require 'report_airborne/rspec_html_formatter'
 
 describe ReportAirborne::RspecHtmlFormatter do
-  it do
-    described_class.new(nil).start(nil)
+  subject { described_class.new(nil) }
+
+  describe '#start' do
+    it 'not raise exception' do
+      expect { subject.start(nil) }.not_to raise_exception
+    end
   end
 
-  it do
-    described_class.new(nil).start(nil)
-    report = double(to_hash: { 'tests' => {}, 'statuses' => {
-                      'passed' => 0, 'failed' => 0, 'pending' => 0
-                    } })
-    allow(ReportAirborne::Report).to receive(:new).and_return(report)
-    described_class.new(nil).stop(nil)
+  describe '#stop' do
+    let(:report) do
+      double(
+        to_hash: {
+          'tests' => {},
+          'statuses' => {
+            'passed' => 0,
+            'failed' => 0,
+            'pending' => 0
+          }
+        }
+      )
+    end
+
+    before { allow(ReportAirborne::Report).to receive(:new).and_return(report) }
+
+    it 'not raise exception' do
+      expect { subject.stop(nil) }.not_to raise_exception
+    end
   end
 end
