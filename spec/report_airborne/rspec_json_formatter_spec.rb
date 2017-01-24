d0c92bcff9e60befb74049928fdc26d081847bb0
d.efimov <d.efimov@fun-box.ru>
2017-01-24 17:57:25 +0400

    improved rspec json formatter

--- spec/report_airborne/rspec_json_formatter_spec.rb
+++ spec/report_airborne/rspec_json_formatter_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 require 'report_airborne/rspec_json_formatter'
 
 describe ReportAirborne::RspecJsonFormatter do
-  it do
-    described_class.new(nil).start(nil)
+  subject { described_class.new(nil) }
+
+  describe '#start' do
+    it 'not raise exception' do
+      expect { subject.start(nil) }.not_to raise_exception
+    end
   end
 
-  it do
-    report = double(to_hash: {})
-    allow(ReportAirborne::Report).to receive(:new).and_return(report)
-    described_class.new(nil).stop(nil)
+  describe '#stop' do
+    let(:report) { double(to_hash: {}) }
+
+    before { allow(ReportAirborne::Report).to receive(:new).and_return(report) }
+
+    it 'not raise exception' do
+      expect { subject.stop(nil) }.not_to raise_exception
+    end
   end
 end
