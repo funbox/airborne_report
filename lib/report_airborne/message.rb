7e1a609a36c87991fe5980aba70c8864479abebf
d.efimov <d.efimov@fun-box.ru>
2017-01-24 16:30:59 +0400

    improved message

--- lib/report_airborne/message.rb
+++ lib/report_airborne/message.rb
@@ -8,35 +8,41 @@ module ReportAirborne
     end
 
     def self.full(request, response)
-      new('time' => Time.now,
-          'request' => {
-            'method' => request.method,
-            'url' => request.url,
-            'headers' => request.headers,
-            'body' => request.args
-          },
-          'response' => {
-            'headers' => response.headers,
-            'body' => MultiJson.load(response)
-          })
+      new(
+        'time' => Time.now,
+        'request' => {
+          'method' => request.method,
+          'url' => request.url,
+          'headers' => request.headers,
+          'body' => request.args
+        },
+        'response' => {
+          'headers' => response.headers,
+          'body' => MultiJson.load(response)
+        }
+      )
     end
 
     def self.wasted(args, response, url)
-      new('time' => Time.now,
-          'request' => {
-            'method' => args[0],
-            'url' => url,
-            'headers' => args[2][:headers],
-            'body' => args[2][:body]
-          },
-          'response' => {
-            'body' => response
-          })
+      new(
+        'time' => Time.now,
+        'request' => {
+          'method' => args[0],
+          'url' => url,
+          'headers' => args[2][:headers],
+          'body' => args[2][:body]
+        },
+        'response' => {
+          'body' => response
+        }
+      )
     end
 
     def self.extra(example)
-      new('full_description' => example.full_description,
-          'status' => example.execution_result.status.to_s)
+      new(
+        'full_description' => example.full_description,
+        'status' => example.execution_result.status.to_s
+      )
     end
 
     def to_hash
--- spec/report_airborne/message_spec.rb
+++ spec/report_airborne/message_spec.rb
@@ -1,7 +1,87 @@
 require 'spec_helper'
 
 describe ReportAirborne::Message do
-  it do
-    described_class.extra(double(full_description: nil, execution_result: double(status: 'qwe')))
+  describe '.new' do
+    it 'not raise exception' do
+      expect { described_class.new({}) }.not_to raise_exception
+    end
+  end
+
+  describe '.full' do
+    let(:request) { double(method: nil, url: nil, headers: nil, args: nil) }
+    let(:response) { double(headers: nil) }
+
+    before do
+      allow(MultiJson).to receive(:load).and_return({})
+      allow(Time).to receive(:now).and_return('coffee time')
+    end
+
+    it 'not raise exception' do
+      expect { described_class.full(request, response) }.not_to raise_exception
+    end
+
+    it 'set date' do
+      expect(described_class.full(request, response).to_hash).to eq(
+        'time' => 'coffee time',
+        'request' => {
+          'method' => nil,
+          'url' => nil,
+          'headers' => nil,
+          'body' => nil
+        },
+        'response' => {
+          'headers' => nil,
+          'body' => {}
+        }
+      )
+    end
+  end
+
+  describe '.wasted' do
+    let(:args) { [nil, nil, {headers: nil, body: nil}] }
+    let(:response) { nil }
+    let(:url) { nil }
+
+    before { allow(Time).to receive(:now).and_return('coffee time') }
+
+    it 'not raise exception' do
+      expect { described_class.wasted(args, response, url) }.not_to raise_exception
+    end
+
+    it 'set date' do
+      expect(described_class.wasted(args, response, url).to_hash).to eq(
+        'time' => 'coffee time',
+        'request' => {
+          'method' => nil,
+          'url' => nil,
+          'headers' => nil,
+          'body' => nil
+        },
+        'response' => {
+          'body' => nil
+        }
+      )
+    end
+  end
+
+  describe '.extra' do
+    let(:example) { double(full_description: nil, execution_result: double(status: nil)) }
+
+    it 'not raise exception' do
+      expect { described_class.extra(example) }.not_to raise_exception
+    end
+
+    it 'set date' do
+      expect(described_class.extra(example).to_hash).to eq(
+        'full_description' => nil,
+        'status' => ''
+      )
+    end
+  end
+
+  describe '#to_hash' do
+    it 'not raise exception' do
+      expect { described_class.new({}) }.not_to raise_exception
+    end
   end
 end
